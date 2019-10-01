6b7c07baeb24a77d8b366c84101ee7744a0671c0
Guilherme Kato <guilhermekato@alunos.utfpr.edu.br>
2019-10-01 16:32:42 -0300

    Feito em aula

---
+++ ArvoresBinarias.h
@@ -0,0 +1,76 @@
+#ifndef ARVORESBINARIAS_ARVORESBINARIAS_H
+#define ARVORESBINARIAS_ARVORESBINARIAS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef struct{
+    int key;
+} Object;
+
+typedef struct NodeTree *PtrTreeTree;
+
+typedef struct NodeTree {
+    Object element;
+    PtrTreeTree left;
+    PtrTreeTree right;
+    PtrTreeTree father; //OPCIONAL
+} NodeTree;
+
+void initTree(PtrTreeTree *node){
+    (*node)= NULL;
+}
+
+bool isEmpty(PtrTreeTree *node){
+    if((*node)== NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool insertItem(PtrTreeTree *node, Object x){
+    if(*node == NULL){
+        (*node) = malloc(sizeof(NodeTree));
+        (*node)->right = (*node)->left = NULL;
+        (*node)->element = x;
+    }
+    if((*node)->element.key == x.key){
+        return false;
+    }
+    if()
+    
+}
+
+int sizeTree(PtrTreeTree *node);
+
+int maxDepth(PtrTreeTree *node);
+
+void destroyTree(PtrTreeTree *node);
+
+void preOrder(PtrTreeTree *node);
+
+void posOrder(PtrTreeTree *node);
+
+void inOrder(PtrTreeTree *node);
+
+
+bool findItem(PtrTreeTree *node, int key, Object *ret);
+
+bool removeItem(PtrTreeTree *node, int key);
+
+PtrTreeTree maximum(PtrTreeTree *node);
+
+PtrTreeTree maximumIterative(PtrTreeTree *node);
+
+PtrTreeTree minimum(PtrTreeTree *node);
+
+PtrTreeTree minimumIterative(PtrTreeTree *node);
+
+PtrTreeTree getMinAux (PtrTreeTree *node); //usado na remoção
+
+PtrTreeTree getMaxAux (PtrTreeTree *node); //usado na remoção
+
+
+#endif //ARVORESBINARIAS_ARVORESBINARIAS_H
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "ArvoresBinarias.h"
+
+
+int main(int argc, char* argv[]) {
+
+    printf("Hello, World!\n");
+    return 0;
+}
